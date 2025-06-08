aa0f4d043de2bac5a7a7cf8ee68679fff3b2f9cc
Nikhil273 <nikhilmaurya.it@gmail.com>
2025-06-08 15:48:39 +0530

    feat: add bcrypt and jsonwebtoken for password hashing and token generation in teacher model

--- src/models/teacher.model.js
+++ src/models/teacher.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const teacherSchema = new mongoose.Schema({
   fulname: {
@@ -54,5 +56,32 @@ const teacherSchema = new mongoose.Schema({
   timestamps: true,
   versionKey: false,
 });
+
+teacherSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  this.next();
+});
+
+teacherSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+teacherSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    { id: this._id, email: this.email },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+teacherSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    { id: this._id, email: this.email },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
+
+
 const Teacher = mongoose.model("Teacher", teacherSchema);
 export default Teacher;
